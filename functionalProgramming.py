d459bd476d0e5e4ba4396794cbb87a7af4f20aa8
Murage <dmurage92@gmail.com>
2021-02-03 08:27:04 +0300

    list comprehensions

--- functionalProgramming.py
+++ functionalProgramming.py
@@ -63,10 +63,46 @@ numbers = range(1, 5)
 xyz = map(operator.mul, letters, numbers)
 # print(' '.join(xyz))
 
-def sum_numbers():
-    thisinp = '10 abc 20 de44 30 55fg 40'.split()
-    for value in thisinp:
-        if value.isdigit():
-            print(value)  ##how to sum these
+# hexnums = '123 abc 1a2b 3f4e'
+# print(sum([int(x, 16)
+#     for x in hexnums.split()]))
 
-sum_numbers()
+# thisinp = '10 abc 20 de44 30 55fg 50'.split()
+# print(sum([int(x) 
+#         for x in thisinp
+#         if x.isdigit()]))
+
+def sum_numbers(numbers):
+    return sum([int(number) 
+        for number in numbers.split() 
+        if number.isdigit()])
+
+# print(sum_numbers(' a sd 34 12 67 90 pn'))
+
+two_levels = [[1,2], [3,4]]
+def flatten_list(multiple_lists):
+    return [num
+            for level in two_levels
+            for num in level]
+
+
+# print(flatten_list([[1,2], [3,4]]))
+def flatten_odd_ints(mylists):
+    return [elem
+        for mylist in mylists
+        for elems in mylist
+        for elem in elems.split()
+        if int(elem.isdigit())
+        if elem/2 != 0]
+
+print(flatten_odd_ints([['1 a c d 32 12'], ['a c 3r 54']]))
+
+# many_levels = [['1 a c d 32 12'], ['a c 3r 54']]
+# for level in many_levels:
+#     for nums in level:
+#         for num in nums.split():
+#             # print(num)
+#             if num.isdigit():
+#                 num = int(num)
+#                 if num/2 != 0:
+#                     print(num)
